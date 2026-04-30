c25cccf2b7b783520eb6eaa931764e75e17e8d78
Jeferson Santana <seuemail@gmail.com>
2026-04-30 12:22:53 -0300

    corrige rota login inicial

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 /* ================================
    CONEXÃO COM SUPABASE
