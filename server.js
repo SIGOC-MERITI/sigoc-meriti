b2ada545b160282038d95b644b1cd47db16d4da1
Jeferson Santana <seuemail@gmail.com>
2026-04-30 12:11:10 -0300

    corrige static express vercel

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 /* ================================
    CONEXÃO COM SUPABASE
